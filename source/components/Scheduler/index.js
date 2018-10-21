d72c3306fe9ef6dc5e6d1a7e036a6dfeb8489597
pixel <taraspixel@gmail.com>
2018-10-21 18:59:34 +0300

    local working lesson 1-8 3

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -1,15 +1,203 @@
 // Core
 import React, { Component } from 'react';
+import moment from 'moment';
+
+// Components
+import Spinner from 'components/Spinner';
+import Task from 'components/Task';
 
 // Instruments
 import Styles from './styles.m.css';
-import { api } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
+//import { api } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
 
 export default class Scheduler extends Component {
+    // constructor () {
+    //     super();
+
+    //     this._createTask = this._createTask.bind(this);
+    //     this._setTasksFetchingState = this._setTasksFetchingState.bind(this);
+    //     // this._removeTask = this._removeTask.bind(this);
+    // }
+
+    state = {
+        tasks: [
+            {
+                id:        '123',
+                message:   'Task 1',
+                created:   1526825076849,
+                favorite:  true,
+                completed: false,
+            },
+            {
+                id:        '456',
+                message:   'Task 2',
+                created:   1526825076849,
+                favorite:  false,
+                completed: false,
+            },
+            {
+                id:        '789',
+                message:   'Task 3',
+                created:   1526825076849,
+                favorite:  false,
+                completed: false,
+            }
+        ],
+        searchQuery:     '',
+        message:         '',
+        isTasksFetching: false,
+    };
+
+    _setTasksFetchingState = (state) => {
+        this.setState({
+            isTasksFetching: state,
+        });
+    }
+
+    _toggleTaskFavoriteState = (id) => {
+        this._setTasksFetchingState(true);
+
+        const newTasks = this.state.task.map((task) => {
+            if (task.id === id) {
+                task.favorite = !task.favorite;
+                console.log("favorite: ", task.favorite);
+            }
+
+            return task;
+        });
+
+        this.setState({
+            tasks:           newTasks,
+            isPostsFetching: false,
+        });
+    }
+
+    _updateTask = () => {
+        this._setTasksFetchingState(true);
+
+        this.setState({
+            isPostsFetching: false,
+        });
+    }
+
+    _createTask = (message) => {
+        this._setTasksFetchingState(true);
+
+        const task = {
+            id:        Math.random().toString(),
+            created:   moment().utc().unix(),
+            message,
+            favorite:  false,
+            completed: false,
+        };
+
+        this.setState(({ tasks }) => ({
+            tasks:           [task, ...tasks],
+            isTasksFetching: false,
+        }));
+    }
+
+    _removeTask = (id) => {
+        this._setTasksFetchingState(true);
+
+        this.setState(({ tasks }) => ({
+            tasks:          tasks.filter((task) => task.id !== id),
+            isTaskFetching: false,
+        }));
+    }
+
+    _updateSearch = (event) => {
+        this.setState({
+            searchQuery: event.target.value,
+        });
+    }
+
+    _updateTaskMessage = (event) => {
+        this.setState({
+            message: event.target.value,
+        });
+    }
+
+    _handleFormSubmit = (event) => {
+        event.preventDefault();
+        this._submitTask();
+    }
+
+    _submitTask = () => {
+        const { message } = this.state;
+
+        if (!message) {
+            return null;
+        }
+
+        this._createTask(message);
+
+        this.setState({
+            message: '',
+        });
+    }
+
+    _submitOnEnter = (event) => {
+        const enterKey = event.key === 'Enter';
+
+        if (enterKey) {
+            event.preventDefault();
+            this._submitTask();
+            console.log('onEnter SHE');
+        }
+    }
+
     render () {
+        const {
+            isTasksFetching,
+            tasks,
+            searchQuery,
+            message,
+        } = this.state;
+
+        const tasksJSX = tasks.map((task) => {
+            return <Task key = { task.id } { ...task } _removeTask = { this._removeTask } />;
+        });
+
         return (
             <section className = { Styles.scheduler }>
-                Планировщик: стартовая точка
+                <Spinner isSpinning = { isTasksFetching } />
+                <main>
+                    <header>
+                        <h1>
+                            Планировщик задач
+                        </h1>
+                        <input
+                            placeholder = 'Поиск'
+                            type = 'search'
+                            value = { searchQuery }
+                            onChange = { this._updateSearch }
+                        />
+                    </header>
+
+                    <section>
+                        <form onSubmit = { this._handleFormSubmit }>
+                            <input
+                                className = 'createTask'
+                                maxLength = { 50 }
+                                placeholder = 'Описaние моей новой задачи'
+                                type = 'text'
+                                value = { message }
+                                onChange = { this._updateTaskMessage }
+                            />
+                            <button>
+                                Добавить задачу
+                            </button>
+                        </form>
+
+                        <div className = 'overlay'>
+                            <ul>
+                                {tasksJSX}
+                            </ul>
+                        </div>
+                    </section>
+
+                </main>
             </section>
         );
     }
--- source/components/Task/index.js
+++ source/components/Task/index.js
@@ -1,10 +1,30 @@
 // Core
 import React, { PureComponent } from 'react';
+//import moment from 'moment';
+import PropTypes from 'prop-types';
 
 // Instruments
 import Styles from './styles.m.css';
 
 export default class Task extends PureComponent {
+    static propTypes = {
+        _removeTask: PropTypes.func.isRequired,
+        //_updateTask: PropTypes.func.isRequired,
+        // _toggleTaskFavoriteState: PropTypes.func.isRequired,
+        completed:   PropTypes.bool.isRequired,
+        created:     PropTypes.number.isRequired,
+        favorite:    PropTypes.bool.isRequired,
+        id:          PropTypes.string.isRequired,
+        message:     PropTypes.string.isRequired,
+    }
+
+    state = {
+        favorite:   this.props.favorite,
+        myRef:      this.props.id,
+        isDisabled: true,
+        message:    this.props.message,
+    }
+
     _getTaskShape = ({
         id = this.props.id,
         completed = this.props.completed,
@@ -17,7 +37,157 @@ export default class Task extends PureComponent {
         message,
     });
 
+    _setTaskFavoriteState (state) {
+        this.setState({
+            favorite: state,
+        });
+        console.log("favorite: ", state);
+    }
+
+    _toggleTaskFavoriteState = () => {
+
+        if (this.state.favorite) {
+            this._setTaskFavoriteState(false);
+        } else {
+            this._setTaskFavoriteState(true);
+        }
+    }
+
+    _setDisabled (state) {
+        this.setState({
+            isDisabled: state,
+        });
+    }
+
+    _removeTask = () => {
+        const { _removeTask, id } = this.props;
+
+        _removeTask(id);
+    }
+
+    _updateTaskMessageOnClick = () => {
+        const ref = this.state.myRef;
+
+        this._setDisabled(false);
+
+        console.log(ref);
+        // SEND DATA
+    }
+
+    _updateTaskMessage = (event) => {
+        this.setState({
+            message: event.target.value,
+        });
+    }
+
+    _updateOnEnter = (event) => {
+        if (event.key === 'Esc') {
+            event.preventDefault();
+            this._setDisabled(true);
+        } else
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            this._setDisabled(true);
+        }
+    }
+
+    _clearUpdates = (event) => {
+        if (event.keyCode === 27) {
+            event.preventDefault();
+            this._setDisabled(true);
+            this.setState({
+                message: this.props.message,
+            });
+            console.log("Esc this.state.message: ", this.state.message);
+            console.log("Esc this.props.message: ", this.props.message);
+        }
+    }
+
+    _getStarStyles = () => {
+        const favorite = this.state.favorite;
+
+        if (favorite) {
+            return `*`;
+        }
+
+        return `-`;
+    }
+
     render () {
-        return <li className = { Styles.task }>Задача: стартовая точка</li>;
+        const {
+            textInput,
+            isDisabled,
+            message,
+        } = this.state;
+
+        const checkbox = {
+            "display": "inline-block",
+            "height":  25,
+            "width":   25,
+        };
+
+        const star = {
+            "display": "inline-block",
+            "height":  19,
+            "width":   19,
+        };
+
+        const edit = {
+            "display": "inline-block",
+            "height":  19,
+            "width":   19,
+        };
+
+        const remove = {
+            "display": "inline-block",
+            "height":  17,
+            "width":   17,
+        };
+
+        const pointer = {
+            "cursor": "pointer",
+        };
+
+        const starStyles = this._getStarStyles();
+
+        return (
+            <li className = { Styles.task }>
+                <div className = { Styles.content }>
+                    <div
+                        className = { Styles.toggleTaskCompletedState }
+                        style = { checkbox }>
+                    [ ]
+                    </div>
+                    <input
+                        disabled = { isDisabled }
+                        maxLength = { 50 }
+                        ref = { textInput }
+                        type = 'text'
+                        value = { message }
+                        onChange = { this._updateTaskMessage }
+                        onKeyDown = { this._clearUpdates }
+                        onKeyPress = { this._updateOnEnter }
+                    />
+                </div>
+
+                <div className = { Styles.actions }>
+                    <div
+                        className = { Styles.toggleTaskFavoriteState }
+                        style = { star }
+                        onClick = { this._toggleTaskFavoriteState }>
+                        <span style = { pointer }>{starStyles}</span>
+                    </div>
+                    <div
+                        className = { Styles.updateTaskMessageOnClick }
+                        style = { edit }
+                        onClick = { this._updateTaskMessageOnClick }>
+                        <span style = { pointer }>[~]</span>
+                    </div>
+                    <div style = { remove } onClick = { this._removeTask } >
+                        <span style = { pointer }>x</span>
+                    </div>
+                </div>
+            </li>
+        );
     }
 }
